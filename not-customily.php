c1526eef03ac5cf07ce83c30c5e6264c43eca1cc
bangnokia <bangnokia@gmail.com>
2023-07-21 17:18:46 +0700

    wip

--- not-customily.php
+++ not-customily.php
@@ -16,7 +16,7 @@
  * Plugin Name:       not customily
  * Plugin URI:        https://github.com/bangnokia
  * Description:       This is not Customily
- * Version:           1.0.0
+ * Version:           0.1.1
  * Author:            Nguyen Viet
  * Author URI:        https://https://daudau.cc
  * License:           GPL-2.0+
