5bd64829054c9aa12e83aaa1358c6247af3af3a0
bangnokia <bangnokia@gmail.com>
2023-10-15 23:05:15 +0700

    fix missing remote css

--- public/class-not-customily-public.php
+++ public/class-not-customily-public.php
@@ -57,8 +57,8 @@ class Not_Customily_Public
     public function enqueue_styles()
     {
 //        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/not-customily-public.css', array(), $this->version, 'all');
-//        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/style.css', array(), $this->version, 'all');
-        wp_enqueue_style($this->plugin_name, 'https://customedge-injector.pages.dev/style.css', array(), $this->version, 'all');
+        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/style.css', array(), $this->version, 'all');
+        wp_enqueue_style($this->plugin_name . '-remote', 'https://customedge-injector.pages.dev/style.css', array(), $this->version, 'all');
     }
 
     public function enqueue_scripts()
